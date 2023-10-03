812095d552e2334e1543a55f7514d182498c3b27
Rubie02 <hoainn9a@gmail.com>
2023-10-03 21:29:45 +0700

    add blogs api

--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
-const { createBlog } = require("../controller/blogController");
+const { createBlog, updateBlog, getBlog, getBlogs, deleteBlog, liketheBlog, disliketheBlog } = require("../controller/blogController");
 const router = express.Router();
 
 router.post('/', authMiddleware, isAdmin, createBlog);
+router.put('/:id', authMiddleware, isAdmin, updateBlog);
+router.get('/:id', getBlog);
+router.get('/', getBlogs);
+router.delete('/:id', authMiddleware, isAdmin, deleteBlog);
+router.put('/likes', authMiddleware, liketheBlog);
+router.put('/dislikes', authMiddleware, disliketheBlog);
 
 module.exports = router;
